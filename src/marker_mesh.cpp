13e367e9bbd511411dc6c1da869c4b1d11c9dae3
Eberty <eberty.silva@hotmail.com>
2018-10-17 16:38:15 -0300

    Added catkin_package() on CMakeLists and the marker is published more efficiently

--- src/marker_mesh.cpp
+++ src/marker_mesh.cpp
@@ -6,18 +6,15 @@
 int main(int argc, char** argv) {
   ros::init(argc, argv, "basic_shapes");
   ros::NodeHandle n;
-  ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise
-      <visualization_msgs::Marker>("visualization_marker", 1);
+      <visualization_msgs::Marker>("visualization_marker", 0);
 
   while (ros::ok()) {
     visualization_msgs::Marker marker;
     marker.header.frame_id = "/my_frame";
-    marker.header.stamp = ros::Time::now();
-
+    marker.header.stamp = ros::Time();
     marker.ns = "basic_shapes";
     marker.id = 0;
-
     marker.type = visualization_msgs::Marker::MESH_RESOURCE;
     marker.action = visualization_msgs::Marker::ADD;
 
@@ -28,19 +25,7 @@ int main(int argc, char** argv) {
     marker.scale.x = 1.0;
     marker.scale.y = 1.0;
     marker.scale.z = 1.0;
-
     marker.mesh_use_embedded_materials = true;
-    marker.lifetime = ros::Duration();
-
-    // Publish the marker
-    while (marker_pub.getNumSubscribers() < 1) {
-      if (!ros::ok()) {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
     marker_pub.publish(marker);
-    r.sleep();
   }
 }
